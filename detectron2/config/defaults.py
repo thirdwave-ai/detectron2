417427406e9fcfbc0bcf84947a8b5ab925c934e1
Daniel Gordon <daniel@thirdwave.ai>
2020-06-10 13:31:24 -0700

    minor changes

--- detectron2/config/defaults.py
+++ detectron2/config/defaults.py
@@ -31,13 +31,17 @@ _C.CLASSES = ['__background__', 'pallet', 'payload', 'person', 'forklift', 'lift
 _C.TWA = CN()
 _C.TWA.DATASET = CN()
 # If using a TWA dataset, configure the TWA.DATASET_DIRECTORY. This is a path relative to the directory on columbia where our datasets are stored: /columbia/MachineLearningAssets/labelbox/
+_C.TWA.MAX_DEPTH_MM = 30000
+
+# Dataset Parameters
 _C.TWA.DATASET.DIRECTORY = ""
 _C.TWA.DATASET.DEBUG = True
 _C.TWA.DATASET.USE_BGR = True
 _C.TWA.DATASET.USE_DEPTH = True
-_C.TWA.MAX_DEPTH_MM = 30000
+
+# Data augmentation parameters
 _C.TWA.DATASET.AUGMENTATION = CN()
-_C.TWA.DATASET.AUGMENTATION.ROTATION = 15
+_C.TWA.DATASET.AUGMENTATION.ROTATION = 20
 
 
 # -----------------------------------------------------------------------------
