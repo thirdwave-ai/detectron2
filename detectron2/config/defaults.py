1eb8554ff3124c00a24f89fcdfdf888818aa68d4
danielgordon10 <daniel@thirdwave.ai>
2020-10-19 16:38:14 -0700

    Update defaults.py

--- detectron2/config/defaults.py
+++ detectron2/config/defaults.py
@@ -59,6 +59,7 @@ _C.TWA.CLASS_THRESHOLDS.frontofpallet = 0.5
 _C.TWA.CLASS_THRESHOLDS.rackface = 0.5
 _C.TWA.CLASS_THRESHOLDS.rackupright = 0.5
 
+_C.TWA.FINAL_WEIGHTS_PATH = ""
 
 # -----------------------------------------------------------------------------
 # END TWA FLAGS- These are flags we have added specific to TWA's use/ standardization.
